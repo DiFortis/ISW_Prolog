e5b2589509093672c3e5a9a4d0ef8dd3bac78561
DiFortis <difortis3@gmail.com>
2024-06-16 01:42:28 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -62,4 +62,4 @@ def wynik():
     return render_template('wynik.html', partia=partia_name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
